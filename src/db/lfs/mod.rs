1fb247af42b7791494e354c5c99ee22ac9bbd03e
Terry <644052732@qq.com>
2024-04-22 21:09:31 +0800

    feat: support mdbx

--- src/db/lfs/mod.rs
+++ src/db/lfs/mod.rs
@@ -14,18 +14,14 @@ pub(crate) enum DBConfig {
         /// Path to the mdbx database
         // path: PathBuf,
         path: String,
+        /// Maximum number of databases
+        max_dbs: usize,
     },
 }
 
 pub(crate) fn open_db(config: DBConfig) -> Result<Box<dyn EigenDB>, ()> {
     match config {
-        DBConfig::Memory => open_memory_db(),
-        DBConfig::Mdbx { .. } => {
-            unimplemented!("open_mdbx_db")
-        }
+        DBConfig::Memory => mem::open_memory_db(),
+        DBConfig::Mdbx { path, max_dbs } => libmdbx::open_mdbx_db(&path, max_dbs),
     }
-}
-
-fn open_memory_db() -> Result<Box<dyn EigenDB>, ()> {
-    Ok(Box::new(mem::Db::default()))
 }
--- src/db/mod.rs
+++ src/db/mod.rs
@@ -11,3 +11,17 @@ pub trait Database {
     fn put(&mut self, key: Vec<u8>, value: Vec<u8>);
     fn del(&mut self, key: Vec<u8>) -> Option<Vec<u8>>;
 }
+
+/// Used to represent different tables or columns or databases
+/// Which is more appropriate to use among tables, columns, and databases to represent our data?
+pub(crate) mod columns {
+    /// The number of columns in the DB
+    pub const TOTAL_COLUMNS: usize = 2;
+
+    /// The column for DEFAULT
+    pub const DEFAULT: usize = 0;
+
+    // TODO: others
+    /// The column for DATA_AVAILABILITY
+    pub const DATA_AVAILABILITY: usize = 1;
+}
