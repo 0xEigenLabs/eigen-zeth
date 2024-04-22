99c242037d3500a6e97bb31feeeeb39e06fa691b
Terry Li <644052732@qq.com>
2024-04-22 21:26:22 +0800

    feat: support mdbx (#7)

--- src/db/lfs/mem.rs
+++ src/db/lfs/mem.rs
@@ -1,3 +1,4 @@
+use crate::db::lfs::mem;
 use crate::db::Database as EigenDB;
 use std::collections::HashMap;
 
@@ -16,4 +17,8 @@ impl EigenDB for Db {
     fn del(&mut self, key: Vec<u8>) -> Option<Vec<u8>> {
         self.0.remove(&key)
     }
+}
+
+pub fn open_memory_db() -> Result<Box<dyn EigenDB>, ()> {
+    Ok(Box::new(mem::Db::default()))
 }
