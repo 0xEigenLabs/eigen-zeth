fc385dec729136543fea219acb17c8db0e3027d7
Terry <644052732@qq.com>
2024-05-02 22:31:01 +0800

    docs: update readme

--- src/config/env.rs
+++ src/config/env.rs
@@ -22,7 +22,7 @@ pub static GLOBAL_ENV: Lazy<GlobalEnv> = Lazy::new(|| GlobalEnv {
     curve_type: std::env::var("CURVE_TYPE").unwrap_or("BN128".to_string()),
     host: std::env::var("HOST").unwrap_or("0.0.0.0:8182".to_string()),
     zeth_db_path: std::env::var("ZETH_DB_PATH")
-        .unwrap_or("/home/terry/project/0xeigen/local-reth-data/tmp/chain".to_string()),
+        .unwrap_or("/tmp/chain".to_string()),
     chain_id: std::env::var("CHAIN_ID")
         .unwrap_or("12345".to_string())
         .parse::<u64>()
