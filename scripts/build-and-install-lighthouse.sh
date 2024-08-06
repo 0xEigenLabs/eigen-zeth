23660935aefb5099a1dad5db4081c431a94739ea
Terry <644052732@qq.com>
2024-08-06 22:21:53 +0800

    chore: install lighthouse with specified commit id

--- scripts/build-and-install-lighthouse.sh
+++ scripts/build-and-install-lighthouse.sh
@@ -5,5 +5,7 @@ cd tmp/lighthouse-source
 rm -rf ./*
 git clone https://github.com/sigp/lighthouse.git
 cd lighthouse
+## lighthouse tag: v5.2.1
+git checkout 9e12c21f268c80a3f002ae0ca27477f9f512eb6f
 echo "Build and Install lighthouse"
 make install
