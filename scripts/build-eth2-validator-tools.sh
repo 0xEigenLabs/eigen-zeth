e8fb5af7b0cc96e4de35a2a8185b8fa67889b296
Terry <644052732@qq.com>
2024-08-10 10:42:20 +0800

    refactor: refactor scripts

--- scripts/build-eth2-validator-tools.sh
+++ scripts/build-eth2-validator-tools.sh
@@ -1,10 +1,26 @@
-cd ..
-echo $PWD
-mkdir -p tmp/tools
-cd tmp/tools
+set -e
+
+CURRENT_PATH=$( cd "$( dirname "$0" )" && pwd )
+PROJECT_PATH=$(dirname "${CURRENT_PATH}")
+echo "PROJECT_PATH: ${PROJECT_PATH}"
+
+INSTALL_PATH="${PROJECT_PATH}/tmp/tools/bin"
+echo "Tools INSTALL_PATH: ${INSTALL_PATH}"
+if [ ! -d "${PROJECT_PATH}/tmp/tools" ]; then
+  mkdir -p "${PROJECT_PATH}/tmp/tools"
+fi
+
+cd "${PROJECT_PATH}/tmp/tools"
+
+echo "Removing existing eth2-val-tools"
 rm -rf ./*
+
+echo "Cloning eth2-val-tools"
 git clone https://github.com/protolambda/eth2-val-tools.git
 cd eth2-val-tools
+
+echo "Checkout eth2-val-tools commit: 0d6d1ddb36479e73d7d876b29ac2d10ab3988e85"
 git checkout 0d6d1ddb36479e73d7d876b29ac2d10ab3988e85
-echo "Build eth2-val-tools, target path: $PWD/tools/bin/eth2-val-tools"
-go build -o ../bin/eth2-val-tools
+
+echo "Build eth2-val-tools, target path: ${INSTALL_PATH}"
+go build -o ${INSTALL_PATH}/eth2-val-tools
--- scripts/generate-eth2-validator-keys.sh
+++ scripts/generate-eth2-validator-keys.sh
@@ -1,12 +1,29 @@
-cd ../tmp
-echo $PWD
+set -e
 
-rm -rf ../testdata/layer2/pos/validator-keys
+CURRENT_PATH=$( cd "$( dirname "$0" )" && pwd )
+PROJECT_PATH=$(dirname "${CURRENT_PATH}")
+echo "PROJECT_PATH: ${PROJECT_PATH}"
 
-tools/bin/eth2-val-tools keystores \
+VALIDATOR_KEYS_PATH="${PROJECT_PATH}/testdata/layer2/pos/validator-keys"
+echo "VALIDATOR_KEYS_PATH: ${VALIDATOR_KEYS_PATH}"
+
+echo "Removing existing validator keys"
+rm -rf ${VALIDATOR_KEYS_PATH}
+
+DEFAULT_MNEMONIC="giant issue aisle success illegal bike spike question tent bar rely arctic volcano long crawl hungry vocal artwork sniff fantasy very lucky have athlete"
+echo "Default mnemonic: ${DEFAULT_MNEMONIC}"
+
+DEFAULT_TOOLS_BIN_PATH="${PROJECT_PATH}/tmp/tools/bin/eth2-val-tools"
+echo "DEFAULT_TOOLS_BIN_PATH: ${DEFAULT_TOOLS_BIN_PATH}"
+if [ ! -f "${DEFAULT_TOOLS_BIN_PATH}" ]; then
+  echo "eth2-val-tools binary not found: ${DEFAULT_TOOLS_BIN_PATH}, please run make build_eth2_validator_tools or scripts/build-eth2-validator-tools.sh"
+  exit 1
+fi
+
+${DEFAULT_TOOLS_BIN_PATH} keystores \
 --insecure \
 --prysm-pass password \
---out-loc ../testdata/layer2/pos/validator-keys \
---source-mnemonic "giant issue aisle success illegal bike spike question tent bar rely arctic volcano long crawl hungry vocal artwork sniff fantasy very lucky have athlete" \
+--out-loc ${VALIDATOR_KEYS_PATH} \
+--source-mnemonic "${DEFAULT_MNEMONIC}" \
 --source-min 0 \
 --source-max 64
