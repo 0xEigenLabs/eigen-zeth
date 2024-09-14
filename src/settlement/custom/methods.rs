da7df52fe1028adbdd50d0e4396df85965414389
BC-A <oneyuanju@gmail.com>
2024-09-15 00:10:28 +0800

    chore: add test

--- src/settlement/custom/methods.rs
+++ src/settlement/custom/methods.rs
@@ -41,6 +41,7 @@ impl CustomClient {
             .send()
             .await;
         let mut last_global_exit_root = [0u8; 32];
+
         match respose {
             Ok(res) => {
                 if res.status().is_success() {
@@ -48,6 +49,7 @@ impl CustomClient {
                     let parsed_json: serde_json::Value = serde_json::from_str(&body).unwrap();
                     let global_exit_root =
                         parsed_json["global_exit_root"].as_str().unwrap_or_default();
+                    log::debug!("global_exit_root: {:?}", global_exit_root);
                     let global_exit_root_bytes =
                         hex::decode(&global_exit_root[2..]).expect("Failed to decode hex string");
 
@@ -74,11 +76,11 @@ impl CustomClient {
     ) -> Result<()> {
         let body = json!({
             "destination_network": destination_network,
-            "destination_address": destination_address.to_string(),
+            "destination_address": destination_address,
             "amount": amount.to_string(),
-            "token": token.to_string(),
+            "token": token,
             "force_update_global_exit_root": force_update_global_exit_root,
-            "calldata": hex::encode(calldata),
+            "calldata": calldata.clone(),
         });
 
         let response = self
@@ -112,9 +114,9 @@ impl CustomClient {
     ) -> Result<()> {
         let body = json!({
             "destination_network": destination_network,
-            "destination_address": destination_address.to_string(),
+            "destination_address": destination_address,
             "force_update_global_exit_root": force_update_global_exit_root,
-            "calldata": hex::encode(calldata),
+            "calldata": calldata,
         });
 
         let response = self
@@ -238,7 +240,7 @@ impl CustomClient {
     pub async fn update_exit_root(&self, network: u32, new_root: [u8; 32]) -> Result<()> {
         let body = json!({
             "network": network,
-            "new_root": new_root
+            "new_root": format!("0x{}", hex::encode(new_root))
         });
 
         let response = self
@@ -304,8 +306,8 @@ impl CustomClient {
             "pending_state_num": pending_state_num,
             "init_num_batch": init_num_batch,
             "final_new_batch": final_new_batch,
-            "new_local_exit_root": hex::encode(new_local_exit_root),
-            "new_state_root": hex::encode(new_state_root),
+            "new_local_exit_root": format!("0x{}", hex::encode(new_local_exit_root)),
+            "new_state_root": format!("0x{}", hex::encode(new_state_root)),
             "proof": proof,
             "input": input
         });
@@ -346,8 +348,8 @@ impl CustomClient {
             "pending_state_num": pending_state_num,
             "init_num_batch": init_num_batch,
             "final_new_batch": final_new_batch,
-            "new_local_exit_root": hex::encode(new_local_exit_root),
-            "new_state_root": hex::encode(new_state_root),
+            "new_local_exit_root": format!("0x{}", hex::encode(new_local_exit_root)),
+            "new_state_root": format!("0x{}", hex::encode(new_state_root)),
             "proof": proof,
             "input": input
         });
@@ -380,6 +382,7 @@ impl CustomClient {
 
 #[cfg(test)]
 mod tests {
+    use self::hex::FromHex;
     use super::*;
     use crate::config::env::GlobalEnv;
     use crate::db::lfs::libmdbx::{open_mdbx_db, Config};
@@ -387,8 +390,7 @@ mod tests {
     use crate::settlement::{init_settlement_provider, NetworkSpec};
     use std::{env, fs};
 
-    #[tokio::test]
-    async fn test_bridge_service() {
+    fn setup() -> Result<Box<dyn Settlement>> {
         let config = CustomSettlementConfig {
             service_url: GLOBAL_ENV.bridge_service_addr.clone(),
         };
@@ -396,65 +398,142 @@ mod tests {
         let settlement_provider = init_settlement_provider(settlement_spec)
             .map_err(|e| anyhow!("Failed to init settlement: {:?}", e))?;
 
-        settlement_provider.bridge_asset(
-            destination_network,
-            destination_address,
-            amount,
-            token,
-            force_update_global_exit_root,
-            calldata,
-        );
-        settlement_provider.bridge_message(
-            destination_network,
-            destination_address,
-            force_update_global_exit_root,
-            calldata,
-        );
-        settlement_provider.claim_asset(
+        Ok(settlement_provider)
+    }
+    #[tokio::test]
+    async fn test_bridge_asset() {
+        let settlement_provider = setup().unwrap();
+        let res = settlement_provider
+            .bridge_asset(
+                1,
+                Address::from_str("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266").unwrap_or_default(),
+                U256::from_str(
+                    "0x0000000000000000000000000000000000000000000000008ac7230489e80000",
+                )
+                .unwrap_or_default(),
+                Address::from_str("0x0165878A594ca255338adfa4d48449f69242Eb8F").unwrap_or_default(),
+                true,
+                Bytes::from_str("0x").unwrap_or_default(),
+            )
+            .await;
+        println!("res: {:?}", res);
+    }
+
+    #[tokio::test]
+    async fn test_claim_asset() {
+        let settlement_provider = setup().unwrap();
+        let hex_strings = [
+            "0x47f0e805088e58e4275879f10276b07b70bf8a009a2e2dbbf3220cab30340fca",
+            "0xad3228b676f7d3cd4284a5443f17f1962b36e491b30a40b2405849e597ba5fb5",
+            "0xb4c11951957c6f8f642c4af61cd6b24640fec6dc7fc607ee8206a99e92410d30",
+            "0x21ddb9a356815c3fac1026b6dec5df3124afbadb485c9ba5a3e3398a04b7ba85",
+            "0xe58769b32a1beaf1ea27375a44095a0d1fb664ce2dd358e7fcbfb78c26a19344",
+            "0x0eb01ebfc9ed27500cd4dfc979272d1f0913cc9f66540d7e8005811109e1cf2d",
+            "0x887c22bd8750d34016ac3c66b5ff102dacdd73f6b014e710b51e8022af9a1968",
+            "0xffd70157e48063fc33c97a050f7f640233bf646cc98d9524c6b92bcf3ab56f83",
+            "0x9867cc5f7f196b93bae1e27e6320742445d290f2263827498b54fec539f756af",
+            "0xcefad4e508c098b9a7e1d8feb19955fb02ba9675585078710969d3440f5054e0",
+            "0xf9dc3e7fe016e050eff260334f18a5d4fe391d82092319f5964f2e2eb7c1c3a5",
+            "0xf8b13a49e282f609c317a833fb8d976d11517c571d1221a265d25af778ecf892",
+            "0x3490c6ceeb450aecdc82e28293031d10c7d73bf85e57bf041a97360aa2c5d99c",
+            "0xc1df82d9c4b87413eae2ef048f94b4d3554cea73d92b0f7af96e0271c691e2bb",
+            "0x5c67add7c6caf302256adedf7ab114da0acfe870d449a3a489f781d659e8becc",
+            "0xda7bce9f4e8618b6bd2f4132ce798cdc7a60e7e1460a7299e3c6342a579626d2",
+            "0x2733e50f526ec2fa19a22b31e8ed50f23cd1fdf94c9154ed3a7609a2f1ff981f",
+            "0xe1d3b5c807b281e4683cc6d6315cf95b9ade8641defcb32372f1c126e398ef7a",
+            "0x5a2dce0a8a7f68bb74560f8f71837c2c2ebbcbf7fffb42ae1896f13f7c7479a0",
+            "0xb46a28b6f55540f89444f63de0378e3d121be09e06cc9ded1c20e65876d36aa0",
+            "0xc65e9645644786b620e2dd2ad648ddfcbf4a7e5b1a3a4ecfe7f64667a3f0b7e2",
+            "0xf4418588ed35a2458cffeb39b93d26f18d2ab13bdce6aee58e7b99359ec2dfd9",
+            "0x5a9c16dc00d6ef18b7933a6f8dc65ccb55667138776f7dea101070dc8796e377",
+            "0x4df84f40ae0c8229d0d6069e5c8f39a7c299677a09d367fc7b05e3bc380ee652",
+            "0xcdc72595f74c7b1043d0e1ffbab734648c838dfb0527d971b602bc216c9619ef",
+            "0x0abf5ac974a1ed57f4050aa510dd9c74f508277b39d7973bb2dfccc5eeb0618d",
+            "0xb8cd74046ff337f0a7bf2c8e03e10f642c1886798d71806ab1e888d9e5ee87d0",
+            "0x838c5655cb21c6cb83313b5a631175dff4963772cce9108188b34ac87c81c41e",
+            "0x662ee4dd2dd7b2bc707961b1e646c4047669dcb6584f0d8d770daf5d7e7deb2e",
+            "0x388ab20e2573d171a88108e79d820e98f26c0b84aa8b2f4aa4968dbb818ea322",
+            "0x93237c50ba75ee485f4c22adf2f741400bdf8d6a9cc7df7ecae576221665d735",
+            "0x8448818bb4ae4562849e949e17ac16e0be16688e156b5cf15e098c627c0056a9",
+        ];
+        let mut smt_proof: [[u8; 32]; 32] = Default::default();
+        for (i, hex_str) in hex_strings.iter().enumerate() {
+            let hex_str = hex_str.trim_start_matches("0x");
+            let bytes = <Vec<u8> as FromHex>::from_hex(hex_str).unwrap_or_default();
+            smt_proof[i].copy_from_slice(&bytes);
+        }
+        let res = settlement_provider.claim_asset(
             smt_proof,
-            index,
-            mainnet_exit_root,
-            rollup_exit_root,
-            origin_network,
-            origin_token_address,
-            destination_network,
-            destination_address,
-            amount,
-            metadata,
-        );
-        settlement_provider.claim_message(
-            smt_proof,
-            index,
-            mainnet_exit_root,
-            rollup_exit_root,
-            origin_network,
-            origin_address,
-            destination_network,
-            destination_address,
-            amount,
-            metadata,
-        );
-        settlement_provider.get_global_exit_root();
-        settlement_provider.get_last_rollup_exit_root();
-        settlement_provider.sequence_batches(batches);
-        settlement_provider.verify_batches(
-            pending_state_num,
-            init_num_batch,
-            final_new_batch,
-            new_local_exit_root,
-            new_state_root,
-            proof,
-            input,
-        );
-        settlement_provider.verify_batches_trusted_aggregator(
-            pending_state_num,
-            init_num_batch,
-            final_new_batch,
-            new_local_exit_root,
-            new_state_root,
-            proof,
-            input,
-        );
-        settlement_provider.update_exit_root(network, new_root);
+            1,
+            <[u8; 32]>::from_hex("0xaadca94ab223b3e975c1cdfed7e2248ab7d91f056ba8f6d9060a36799f950a7e".trim_start_matches("0x")).expect("Invalid hex string"),
+            <[u8; 32]>::from_hex("0xaadca94ab223b3e975c1cdfed7e2248ab7d91f056ba8f6d9060a36799f950a7e".trim_start_matches("0x")).expect("Invalid hex string"),
+            0,
+            Address::from_str("0x0165878A594ca255338adfa4d48449f69242Eb8F").unwrap_or_default(),
+            1,
+            Address::from_str("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266").unwrap_or_default(),
+            U256::from_str("0x0000000000000000000000000000000000000000000000008ac7230489e80000").unwrap_or_default(),
+            Bytes::from_str("0x000000000000000000000000000000000000000000000000000000000000006000000000000000000000000000000000000000000000000000000000000000a00000000000000000000000000000000000000000000000000000000000000012000000000000000000000000000000000000000000000000000000000000000b456967656e20546f6b656e0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000005454947454e000000000000000000000000000000000000000000000000000000").unwrap_or_default(),
+        ).await;
+
+        println!("res: {:?}", res);
+    }
+    #[tokio::test]
+    async fn test_get_global_exit_root() {
+        let settlement_provider = setup().unwrap();
+        let res = settlement_provider.get_global_exit_root().await;
+        println!("res: {:?}", res);
+    }
+
+    // async fn test_get_last_rollup_exit_root() {
+    //     let (settlement_provider, setup_params) = setup().unwrap();
+    //     settlement_provider.get_last_rollup_exit_root();
+    // }
+
+    #[tokio::test]
+    async fn test_sequence_batches() {
+        let settlement_provider = setup().unwrap();
+        let batches = vec![BatchData {
+            transactions: vec![1],
+            global_exit_root: <[u8; 32]>::from_hex(
+                "0xaadca94ab223b3e975c1cdfed7e2248ab7d91f056ba8f6d9060a36799f950a7e"
+                    .trim_start_matches("0x"),
+            )
+            .expect("Invalid hex string"),
+            timestamp: 111111111,
+        }];
+        let res = settlement_provider.sequence_batches(batches).await;
+        println!("res: {:?}", res);
+    }
+
+    #[tokio::test]
+    async fn test_verify_batches() {
+        let settlement_provider = setup().unwrap();
+        let res = settlement_provider.verify_batches(
+            0,
+            0,
+            1,
+            <[u8; 32]>::from_hex("0xaadca94ab223b3e975c1cdfed7e2248ab7d91f056ba8f6d9060a36799f950a7e".trim_start_matches("0x")).expect("Invalid hex string"),
+            <[u8; 32]>::from_hex("0xaadca94ab223b3e975c1cdfed7e2248ab7d91f056ba8f6d9060a36799f950a7e".trim_start_matches("0x")).expect("Invalid hex string"),
+            "{\"pi_a\":{\"x\":\"9898772573056133167685730031820439200779677251915533133089180906078340598153\",\"y\":\"4988585277003916341444053304584863613990493280622140059976827625686886539523\"},\"pi_b\":{\"x\":[\"20509617804647477524035568417927205406397155061230874893680539238495862881580\",\"6585671937830049132098354513544251392871629080955562897302125989145227322378\"],\"y\":[\"16600973708004378995096185526104442264785941566440480473110565973138301268148\",\"4748797935380146483285828556664955484346426282325689850322414929184461946949\"]},\"pi_c\":{\"x\":\"13887335001047178786869303446566979485698626327666217004092778514366245776228\",\"y\":\"14978410198885453128070499586881293125336387926725723435864499846753605937804\"},\"protocol\":\"groth16\",\"curve\":\"BN128\"}".to_string(),
+            "[\"8362010813876620668586816087691982027748700171077865203929785134913673568689\"]".to_string(),
+        ).await;
+        println!("res: {:?}", res);
+    }
+    #[tokio::test]
+
+    async fn test_update_exit_root() {
+        let settlement_provider = setup().unwrap();
+        // only bridgeAddress or rollupAddress can call
+        let res = settlement_provider
+            .update_exit_root(
+                1,
+                <[u8; 32]>::from_hex(
+                    "0xaadca94ab223b3e975c1cdfed7e2248ab7d91f056ba8f6d9060a36799f910a7e"
+                        .trim_start_matches("0x"),
+                )
+                .expect("Invalid hex string"),
+            )
+            .await;
+        println!("res: {:?}", res);
     }
 }
